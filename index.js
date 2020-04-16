c1710d4dd81e1d9833ef66f4a2a184d927a167cc
henrik9999 <22085664+henrik9999@users.noreply.github.com>
2020-04-16 19:19:28 +0200

    initial backup action

---
+++ index.js
@@ -0,0 +1,19 @@
+const fetch = require('node-fetch');
+const fs = require('fs');
+const path = require('path');
+
+fetch('https://kissanimelist.firebaseio.com/Data2.json?auth=TOKEN')
+.then(res => {
+	if(res.status !== 200) {
+		throw "response status not 200";
+	}
+	return res;
+})
+.then(res => res.json())
+.then(json => fs.writeFileSync(path.resolve("firebase.json"),JSON.stringify(json, null, 2)))
+.catch(() => {throw "error"});
+
+process.on('unhandledRejection', err => {
+	console.error(err);
+	process.exit(1);
+});
