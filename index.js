8cbe70a84a430191f576687b3a20b7c153134ecb
lolamtisch <francisco.seipel@yahoo.de>
2020-10-15 15:08:30 +0200

    [TASK] Get Backup from new db

--- index.js
+++ index.js
@@ -2,16 +2,23 @@ const fetch = require('node-fetch');
 const fs = require('fs');
 const path = require('path');
 
-fetch('https://kissanimelist.firebaseio.com/Data2.json?auth='+process.env.FIREBASE_TOKEN)
-.then(res => {
-	if(res.status !== 200) {
-		throw "response status not 200";
-	}
-	return res;
-})
-.then(res => res.json())
-.then(json => fs.writeFileSync(path.resolve("firebase.json"),JSON.stringify(json, null, 2)))
-.catch(() => {throw "error"});
+download('/mal/export', 'mal').then( () => download('/page/export', 'page'))
+
+async function download(url, filename) {
+	fetch(process.env.DOMAIN + url, {
+		headers: {
+			'x-access-token': process.env.ACCESS_TOKEN
+		}
+	}).then(res => {
+		if(res.status !== 200) {
+			throw "response status not 200";
+		}
+		return res;
+	})
+	.then(res => res.json())
+	.then(json => fs.writeFileSync(path.resolve(filename + ".json"),JSON.stringify(json, null, 2)))
+	.catch(() => {throw "error"});
+}
 
 process.on('unhandledRejection', err => {
 	console.error(err);
