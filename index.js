7f33beb55179268d3c7d80be1e3450a61e6db497
henrik9999 <22085664+henrik9999@users.noreply.github.com>
2021-06-14 20:21:37 +0200

    update

--- index.js
+++ index.js
@@ -1,47 +1,62 @@
 const fetch = require('node-fetch');
-const fs = require('fs');
+const fs = require('fs-extra')
 const path = require('path');
 const table = require('markdown-table');
 
 async function start() {
-	const pageData = convertToPage(await getPaginatedData('/page/export'));
-	fs.writeFileSync(path.resolve('page' + ".json"),JSON.stringify(pageData, null, 2));
-
-	const malData = convertToMal(await getPaginatedData('/mal/export'));
-	fs.writeFileSync(path.resolve('mal' + ".json"),JSON.stringify(malData, null, 2));
-
+	fs.removeSync('data');
+	await backup('/page/export', 'pages');
+	await backup('/mal/export', 'myanimelist');
+	await backup('/ani/export', 'anilist');
 	await updateReadme();
 }
 
-function convertToMal(data) {
-	const malData = {};
-	for (const entry of data) {
-		if(!malData[entry.type]) {
-			malData[entry.type] = {};
+
+async function backup(url, name, type = 'anime', page = 0, ids = {}) {
+	await getData(url + '/' + type + '/' + page).then(async (json) => {
+		const promises = json.data.map(data => {
+			if(!ids[data.page ?? type]) {
+				ids[data.page ?? type] = [];
+			}
+			ids[data.page ?? type].push(data.identifier ?? data.id);
+			return fs.outputFile(`data/${name}/${data.page ?? type}/${data.identifier ?? data.id}.json`, JSON.stringify(data))
+		})
+		await Promise.all(promises);
+
+		if(json.next) {
+			return await backup(url, name, type, page + 1, ids);
+		} else {
+			for(item in ids) {
+				fs.outputFile(`data/${name}/${item}/_index.json`, JSON.stringify(ids[item]))
+			}
+			if(type === 'anime') {
+				return await backup(url, name, 'manga')
+			}
+			return true;
 		}
-		malData[entry.type][entry.id] = entry;
-	}
-	return malData;
+	}).catch((e) => {throw 'backup error'});
 }
 
-function convertToPage(data) {
-	const pageData = {};
-	for (const page of data) {
-		if(!pageData[page.page]) {
-			pageData[page.page] = {};
+async function getData(url) {
+	return fetch(process.env.DOMAIN + url, {
+		headers: {
+			'x-access-token': process.env.ACCESS_TOKEN
 		}
-		pageData[page.page][page.identifier] = page;
-	}
-	return pageData;
+	}).then(res => {
+		if(res.status !== 200) {
+			throw "response status not 200";
+		}
+		return res.json()
+	})
 }
 
 async function updateReadme() {
 	getData('/stats/db').then(json => {
-		let stats = [['Page', 'Total', 'Malids', 'Empty']];
+		let stats = [['Page', 'Total', 'MalID', 'noMalID', 'AniID', 'noAniID']];
 
 		for (const pageName in json.pages) {
 			const page = json.pages[pageName];
-			stats.push([pageName, page.total, page.mal, page.noMal])
+			stats.push([pageName, page.total, page.mal, page.noMal, page.ani, page.noAni])
 		}
 
 		const statstable = table(stats);
@@ -58,39 +73,6 @@ async function updateReadme() {
 			});
 		});
 	});
-}
-
-let data = {};
-async function getPaginatedData(url, type = 'anime', page = 0) {
-	return await getData(url + '/' + type + '/' + page).then(async (json) => {
-		if(data[url]) {
-			data[url] = [].concat(data[url], json.data);
-		} else {
-			data[url] = json.data;
-		}
-		if(json.next) {
-			return await getPaginatedData(url, type, page + 1);
-		} else {
-			if(type === 'anime') {
-				return await getPaginatedData(url, 'manga')
-			} else {
-				return data[url];
-			}
-		}
-	}).catch((e) => {throw "pagination error"});
-}
-
-async function getData(url) {
-	return fetch(process.env.DOMAIN + url, {
-		headers: {
-			'x-access-token': process.env.ACCESS_TOKEN
-		}
-	}).then(res => {
-		if(res.status !== 200) {
-			throw "response status not 200";
-		}
-		return res.json()
-	})
 }
 
 process.on('unhandledRejection', err => {
